efec8ab1371fd97a3fb9c54075099719edf81d92
Andragon <oscarcamarena31@hotmail.com>
2026-03-27 07:57:31 -0600

    chore: bump install.ps1 version to v1.4.1

--- install.ps1
+++ install.ps1
@@ -1,14 +1,14 @@
-# Ragnarok Installer v1.4.0
+# Ragnarok Installer v1.4.1
 # AI Governance & Memory Layer Ecosystem
 # Usage: 
-#   irm https://raw.githubusercontent.com/andragon31/Ragnarok/v1.4.0/install.ps1 | iex
+#   irm https://raw.githubusercontent.com/andragon31/Ragnarok/v1.4.1/install.ps1 | iex
 #   Or download and run manually
 
 param(
     [string]$InstallDir = "$env:LOCALAPPDATA\Ragnarok"
 )
 
-$VERSION = "1.4.0"
+$VERSION = "1.4.1"
 $REPO_URL = "https://github.com/andragon31/Ragnarok"
 
 # Save script to temp if running from remote (irm | iex)
